7758755dd642cda78d8a9b40b5b553cf085bc0f3
AlexAlestra100 <58407966+AlexAlestra100@users.noreply.github.com>
2020-12-08 16:43:40 -0800

    Add files via upload

---
+++ Starting C/lab1.c
@@ -0,0 +1,17 @@
+/*----------------*/
+/*Alexander Alestra*/
+/*Lab 1*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	printf("\nLab 1\n\n");
+	printf("Hello World.\n");
+	printf("Hi, Alexander Alestra.\n\n");
+	printf("Failure is progress.\n-Unknown\n\n");
+
+	return EXIT_SUCCESS;
+}
+/*----------------*/
