28b32e17656d091c463e228c458ee96d42e0efa2
AlexAlestra100 <58407966+AlexAlestra100@users.noreply.github.com>
2021-01-31 22:45:54 -0800

    Add files via upload

---
+++ Assembly Language & Transistors/double1(mine)/exp10to2.c
@@ -0,0 +1,42 @@
+#include "number.h"
+#include "exp10to2.h"
+
+void exp10ToExp2(struct Number *pn)
+{
+	if (pn->exp10 < 0);
+	{
+	    while (pn->exp10 < 0)
+        {
+            while (pn->mantissa <= (UINT64_MAX) / 2)
+            {
+                pn->mantissa = pn->mantissa * 2;
+                pn->exp2--;
+            }
+            pn->mantissa = (pn->mantissa + 5)/10;
+            pn->exp10++;
+            while (pn->mantissa <= (UINT64_MAX) / 2)
+            {
+                pn->mantissa = pn->mantissa * 2;
+                pn->exp2--;
+            }
+        }
+	}
+	if (pn->exp10 > 0)
+    {
+        while (pn->exp10 > 0)
+        {
+            while ((pn->mantissa <= (UINT64_MAX) / 10) && (pn->exp10 > 0))
+            {
+                pn->mantissa = pn->mantissa * 10;
+                pn->exp10--;
+            }
+            /*pn->mantissa = (pn->mantissa + 5) / 2;
+            pn->exp2++;
+            while (pn->mantissa <= (UINT64_MAX) / 2)
+            {
+                pn->mantissa = pn->mantissa / 2;
+                pn->exp2++;
+            }*/
+        }
+    }
+}
---
+++ Assembly Language & Transistors/double1(prof)/exp10to2.c
@@ -0,0 +1,32 @@
+#include "number.h"
+#include "exp10to2.h"
+
+void exp10ToExp2(struct Number *pn)
+{
+	if (pn->exp10 < 0);
+	{
+	    while (pn->exp10 > 0)
+        {
+            while (pn->mantissa < UINT64_MAX)
+            {
+                pn->mantissa = pn->mantissa * 2;
+                pn->exp2--;
+            }
+            pn->mantissa = (pn->mantissa + 5)/10;
+            pn->exp10++;
+        }
+	}
+	if (pn->exp10 > 0)
+    {
+        while (pn->exp10 < 0)
+        {
+            while (pn->mantissa < UINT64_MAX)
+            {
+                pn->mantissa = pn->mantissa * 2;
+                pn->exp2++;
+            }
+            pn->mantissa = (pn->mantissa + 5)/10;
+            pn->exp10--;
+        }
+    }
+}
