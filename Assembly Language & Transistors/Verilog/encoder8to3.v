d2bcca20f7681c1481311c8e3c913f890ef2c2e0
AlexAlestra100 <58407966+AlexAlestra100@users.noreply.github.com>
2021-04-06 22:44:34 -0700

    Update encoder8to3.v

--- Assembly Language & Transistors/Verilog/encoder8to3.v
+++ Assembly Language & Transistors/Verilog/encoder8to3.v
@@ -1,8 +1,3 @@
-// Alexander Alestra
-// encoder8to3.v, 8-to-3 encoder, gate synthesis
-// compile: iverilog encoder8to3.v
-// run: ./a.out
-
 module EncoderMod(i0, i1, i2, i3, i4, i5, i6, i7, o2, o1, o0);
    input i0, i1, i2, i3, i4, i5, i6, i7;
    output o2, o1, o0;
