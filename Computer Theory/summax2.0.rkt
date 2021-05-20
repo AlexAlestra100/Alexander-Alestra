65fddfb54dc18320c664648362fe7106be3a561e
AlexAlestra100 <58407966+AlexAlestra100@users.noreply.github.com>
2021-05-19 19:10:13 -0700

    Create summax2.0.rkt

---
+++ Computer Theory/summax2.0.rkt
@@ -0,0 +1,14 @@
+#lang racket
+(provide summax)
+; do not change any code above this line. Write your code below it.
+
+; summax: sum of max(f(x), g(x)) for each x in xs
+; f: object --> integer
+; g: object --> integer
+; xs: list of objects
+; (summax add1 abs '(2 -2 -3 3)) evaluates to 12
+; (summax add1 abs '()) evaluates to 0
+(define (summax f g xs)
+    (if (empty? xs)
+    0
+    (foldl (lambda (s acc) (+ acc (max (f s) (g s)))) 0 xs)))
