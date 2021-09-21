92fc3335dcca6cff4f64d552921dd13b9838f000
AlexAlestra100 <58407966+AlexAlestra100@users.noreply.github.com>
2021-09-20 23:37:06 -0700

    Update observe.c

--- OS/HardwareInfo/observe.c
+++ OS/HardwareInfo/observe.c
@@ -8,280 +8,220 @@
 #include <time.h>
 #include <string.h>
 
+FILE *ap;
+
+char cpuinfo[] = "/proc/cpuinfo";
+char hostname[] = "/proc/sys/kernel/hostname";
+char loadavg[] = "/proc/loadavg";
+char meminfo[] = "/proc/meminfo";
+char partitions[] = "/proc/partitions";
+char stat[] = "/proc/stat";
+char uptime[] = "/proc/uptime";
+char version[] = "/proc/version";
+
 int partB();
 int partC();
 int partD(int i, int d);
 
-int main(int argc, char *argv[])
-{
-    char c1, c2;
-    int interval, duration;
+int main(int argc, char *argv[]){
+   char c1, c2;
+   int interval, duration;
 
-    printf("\nPart B\n------\n");
-    partB();
-    
-    if(argc > 1)
-    {
-        sscanf(argv[1], "%c%c", &c1, &c2);
+   partB();
+
+   if(argc > 1){
+      sscanf(argv[1], "%c%c", &c1, &c2);
         
-        if (c1 != '-')
-        {
-            fprintf(stderr, "usage: observer [-s][-l int dur]\n");
-            exit(1);
-        }
+      if(c1 != '-'){
+         fprintf(stderr, "usage: observer [-s][-l int dur]\n\n");
+         exit(1);
+      }
 
-        if(c2 == 's')
-        {
-            printf("Part C\n------\n");
+      if(c2 == 's'){
             partC();
-        }
-        if(c2 == 'l')
-        {
-            interval = atoi(argv[2]);
-            duration = atoi(argv[3]);
-            partD(interval, duration);
-        }
-    }
-    
-    return 0;
+      }
+
+      if(c2 == 'l'){
+         partC();
+         interval = atoi(argv[2]);
+         duration = atoi(argv[3]);
+         partD(interval, duration);
+      }
+   }
+   
+   return 0;
 }
 
-int partD(int i, int d)
-{
-    printf("Part D\n------\n");
+int partB(){
+    printf("\nPart B\n------\n");
 
-    FILE *mem;
-    char avmem[29];
-    char inmem[29];
-    
-    mem = fopen("/proc/meminfo", "r");
+   char cpuModel[100];
+   char host[30];
+   char linuxV[42];
+   int days, hours, minutes, seconds, upsec;
+   double uptime1 = 0;
+   time_t now = time(NULL);
+   struct tm tm = *localtime(&now);
+   
+   ap = fopen(cpuinfo, "r");
+   if(ap){
+      int i;
 
-    if(mem)
-    {
-        fgets(inmem, 29, mem);
-        fgets(avmem, 29, mem); 
-          
-        printf("\n%s\n%s\n", inmem, avmem);
+      for(i = 0; i < 5; i++){
+         fgets(cpuModel, 100, ap);
+      }
 
-        fclose(mem);
+      printf("\n%s\n", cpuModel);
+      fclose(ap);
+   }
+   else{
+      fclose(ap);
+      printf("Unable to open file %s!\n", cpuinfo);
+   }
 
-    }
-    else
-    {
-        printf("Unable to open file!\n");
-    }
+   ap = fopen(version, "r");
+   if(ap){
+      fgets(linuxV, 42, ap);
+      printf("%s\n", linuxV);
+      fclose(ap);
+   }
+   else{
+      fclose(ap);
+      printf("Unable to open file %s!\n", version);
+   }
+   
+   ap = fopen(uptime, "r");
+   if(ap){
+      fscanf(ap, "%lf", &uptime1);
+      upsec = uptime1;
+      days = upsec / 86400;
+      hours = (upsec % 86400) / 3600;
+      minutes = (upsec % 3600) / 60;
+      seconds = (upsec % 60); 
+      
+      printf("\nTime since last reboot: (%d:%d:%d:%d)\n", days, hours, minutes, seconds);
 
-    FILE *avg;
-    char load[50];
+      fclose(ap);
+   }
+   else{
+      fclose(ap);
+      printf("Unable to open file %s!\n", uptime);
+   }
 
-    printf("Load Averages\nIntervals: %d seconds\nDuration: %d seconds\n\n", i, d);
+   printf("\nCurrent Time: %02d:%02d:%02d\n", tm.tm_hour, tm.tm_min, tm.tm_sec);
+   ap = fopen(hostname, "r"); 
+   if(ap){
+      fgets(host, 30, ap);
+      printf("\nMachine Host Name: %s\n", host);
+      fclose(ap);
+   }
+   else{
+      fclose(ap);
+      printf("Unable to open file %s!\n", hostname);
+   }
 
-    int iter = 0;
-
-    while(iter < d)
-    {
-        avg = fopen("/proc/loadavg", "r");
-        fgets(load, 50, avg);
-        fclose(avg);
-
-        sleep(i);
-        printf("%s\n", load);
-        iter += i;
-    }
-
-    return 0;
+   return 0;
 }
 
-int partC()
-{
-    FILE *pt;
-    char temp[10];
-    int fuser, nuser, systm, idle;
+int partC(){
+   printf("Part C\n------\n");
 
-    char stat[] = "/proc/stat"; 
+   char pros[17];
+   char rw[20];
+   char swit[15];
+   char temp[10];
+   int fuser, nuser, systm, idle;
 
-    pt = fopen(stat,  "r");
+   ap = fopen(stat,  "r");
+   if(ap){ 
+      fscanf(ap, "%s%d%d%d%d", temp, &fuser, &nuser, &systm, &idle);
+      printf("\nUser mode: %d\nSystem mode: %d\nIdle: %d\n", (fuser + nuser), systm, idle);
+      fclose(ap);
+   }
+   else{
+      fclose(ap);
+      printf("\nUnable to open file %s.\n", stat);   
+   }
 
-    if(pt)
-    { 
-        fscanf(pt, "%s%d%d%d%d", temp, &fuser, &nuser, &systm, &idle);
-        printf("\nUser mode: %d\nSystem mode: %d\nIdle: %d\n", (fuser + nuser), systm, idle);
-        fclose(pt);
-    }
-    else
-    {
-        printf("\nCould not open file %s.\n", stat);   
-    }
+   ap = fopen(partitions, "r");
+   if(ap){ 
+      int i;
+      for(i = 0; i < 11; i++)
+      {
+         fscanf(ap, "%s", rw);
+      }
+      printf("\nDisk read/write requests: %s\n", rw);
+      fclose(ap);
+   }
+   else{
+      fclose(ap);
+      printf("\nUnable to open file %s.\n", partitions);   
+   }
+   
+   ap = fopen(stat, "r");
+   if(ap){
+      int j;
+      
+      for(j = 0; j < 81; j++){
+         fgets(swit, 15, ap);
+      }
 
-    char mem[] = "/proc/partitions";
+      printf("\nContext Switches: %s\n", swit);
+      fclose(ap);
+   }
+   else{
+      fclose(ap);
+      printf("Unable to open file %s\n", stat);
+   }
 
-    pt = fopen(mem, "r");
-    
-    char rw[20];
+   ap = fopen(stat, "r");
+   if(ap){
+      int k;
 
-    if(pt)
-    { 
-        int i;
-        for(i = 0; i < 11; i++)
-        {
-            fscanf(pt, "%s", rw);
-        }
-        printf("\nDisk read/write requests: %s\n", rw);
-        fclose(pt);
-    }
-    else
-    {
-        printf("\nCould not open file %s.\n", mem);   
-    }
+      for(k = 0; k < 75; k++){   
+         fgets(pros, 17, ap);
+      }
 
-    char stat2[] = "/proc/stat";
-    char swit[15];
-    char pros[17];
-    
-    pt = fopen(stat2, "r");
+      printf("\nProcesses since boot: %s\n\n", pros);
+      fclose(ap);
+   }
+   else{
+      fclose(ap);
+      printf("Unable to open file %s\n", stat);
+   }
 
-    if(pt)
-    {
-        int j;
-        
-        for(j = 0; j < 81; j++)
-        {
-            fgets(swit, 15, pt);
-        }
-
-        printf("\nContext Switches: %s\n", swit);
-
-        fclose(pt);
-    }
-    else
-    {
-        printf("Could not open file %s\n", stat2);
-    }
-
-    pt = fopen(stat2, "r");
-
-    if(pt)
-    {
-        int j;
-
-        for(j = 0; j < 75; j++)
-        {   
-            fgets(pros, 17, pt);
-        }
-
-        printf("\nProcesses since boot: %s\n\n", pros);
-
-        fclose(pt);
-    }
-    else
-    {
-        printf("Could not open file %s\n", stat2);
-    }
-
-    return 0;
+   return 0;
 }
 
-int partB()
-{
-    FILE *fp;
-    FILE *fp1;
-    FILE *fp2;
-    char cpuModel[100];
-    char linuxV[42];
-    double uptime = 0;
-    
-    fp = fopen("/proc/cpuinfo", "r");
-    
-    if(fp)
-    {
-        int i;
+int partD(int i, int d){
+   printf("Part D\n------\n");
 
-        for(i = 0; i < 5; i++)
-        {
-            fgets(cpuModel, 100, fp);
-        }
+   char avmem[29];
+   char inmem[29];
+   char load[50];
+   int iter = 0;
 
-        printf("\n%s\n", cpuModel);
+   ap = fopen(meminfo, "r");
+   if(ap){
+      fgets(inmem, 29, ap);
+      fgets(avmem, 29, ap);
+      printf("\n%s\n%s\n", inmem, avmem);
+      fclose(ap);
+   }
+   else{
+      fclose(ap);
+      printf("Unable to open file %s!\n", meminfo);
+   }
 
-        fclose(fp);
+   printf("Load Averages\nIntervals: %d seconds\nDuration: %d seconds\n\n", i, d);
+   while(iter < d){
+      ap = fopen(loadavg, "r");
+      fgets(load, 50, ap);
+      fclose(ap);
+      sleep(i);
+      printf("%s\n", load);
+      iter += i;
+   }
 
-    }
-    else
-    {
-        printf("Unable to open file!\n");
-    }
-
-    fp1 = fopen("/proc/version", "r");
-
-    if(fp1)
-    {
-
-        fgets(linuxV, 42, fp1);
-       
-        printf("%s\n", linuxV);
-
-        fclose(fp1);
-
-
-    }
-    else
-    {
-        printf("Unable to open file!\n");
-    }
-    
-    fp2 = fopen("/proc/uptime", "r");
-    
-    int days, hours, minutes, seconds; 
-    int upsec; 
-    
-    if(fp2)
-    {
-
-        fscanf(fp2, "%lf", &uptime);
-        
-        upsec = uptime;
-
-        days = upsec / 86400;
-
-        hours = (upsec % 86400) / 3600;
-        
-        minutes = (upsec % 3600) / 60;
-        
-        seconds = (upsec % 60); 
-          
-        printf("\nTime since last reboot: (%d:%d:%d:%d)\n", days, hours, minutes, seconds);
-
-        fclose(fp2);
-
-    }
-    else
-    {
-        printf("Unable to open file!\n");
-    }
-    
-    time_t now = time(NULL);
-    struct tm tm = *localtime(&now);
-    printf("\nCurrent Time: %02d:%02d:%02d\n", tm.tm_hour, tm.tm_min, tm.tm_sec);
-
-    fp1 = fopen("/proc/sys/kernel/hostname", "r");
-
-    char host[30]; 
-    
-    if(fp1)
-    {
-
-        fgets(host, 30, fp1);
-       
-        printf("\nMachine Host Name: %s\n", host);
-
-        fclose(fp1);
-
-
-    }
-    else
-    {
-        printf("Unable to open file!\n");
-    }
-
-    return 0;
+   return 0;
 }
